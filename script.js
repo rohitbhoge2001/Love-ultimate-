d59b4ec4006aec1cd7a901a1d0b85bec8a377705
rohitbhoge2001 <ROHITBHOGE12@GMAIL.COM>
2025-03-01 13:56:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startLoveGame() {
+    alert("Love game started! ❤️");
+}
+
+function checkAnswer(answer) {
+    if (answer === 'A') {
+        alert("Correct! This is our special date! 💕");
+    } else {
+        alert("Oops! Try again! 😘");
+    }
+}
